c639c59567809f8f72178c098be55a02e02ffb31
Lu Vy <luvy@umich.edu>
2025-01-01 14:35:18 -0700

    Update plotPrices.py

--- dynamic-information/plotPrices.py
+++ dynamic-information/plotPrices.py
@@ -13,36 +13,20 @@ lam = np.load('data/lam.npy')
 SIG = np.load('data/SIG.npy')
 N = np.load('data/N.npy')
 
-yy = np.linspace(-2.5, 2.5, 501); dy = norm.pdf(x = yy)
-pp = np.linspace(p0 - 2.5 * lam, p0 + 2.5 * lam, 501)
-dp = norm.pdf(x = pp, loc = p0, scale = np.sqrt(lam));
-
-ub = P + 1.96 * lam * np.sqrt(SIG)
-lb = P - 1.96 * lam * np.sqrt(SIG)
-
-
 fig, ax = plt.subplots(nrows = 2, ncols = 1,
                        figsize = (4.5,3.5), dpi = 256,
-                      sharex = 'col')
-
-ymin = lb.min() - 0.25
-ymax = ub.max() + 0.25
+                      sharex = 'col', sharey = True)
 
 K = int(0.75 * len(t))
 
-ax[0].set_ylim(bottom = ymin, top = ymax)
 ax[0].set_xlim(left = -0.05, right = 1.05)
-ax[0].set_ylabel('price')
-ax[0].set_ylim(bottom = ymin, top = ymax)
-ax[0].text(0,9.5,'what the informed trader sees', color = 'gray')
+ax[0].set_ylabel('what the informed \n trader sees', fontsize = 'small')
 ax[0].plot(t[:K], P[:K], label = 'market price', c = 'C0')
 ax[0].plot(t[:K], V[:K], label = 'true price', c = 'C2')
 ax[0].legend(fontsize = 'xx-small', loc = 'lower right')
 
-ax[1].set_ylim(bottom = ymin, top = ymax)
 ax[1].plot(t[:K], P[:K], label = 'market price', c = 'C0')
-ax[1].text(0,9.5,'what the market maker sees', color = 'gray')
-ax[1].set_ylabel('price')
+ax[1].set_ylabel('what the market \n maker sees', fontsize = 'small')
 ax[1].set_xlabel('time')
 ax[1].legend(fontsize = 'xx-small', loc = 'lower right')
 
