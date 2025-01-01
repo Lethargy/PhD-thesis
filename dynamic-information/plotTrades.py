a926f740e959ab36c7351d9a7b671594cf68ca4b
Lu Vy <luvy@umich.edu>
2025-01-01 14:47:18 -0700

    Update plotTrades.py

--- dynamic-information/plotTrades.py
+++ dynamic-information/plotTrades.py
@@ -18,12 +18,11 @@ ax[0].plot(t[:K], X[:K], label = 'informed trades', c = 'C1')
 ax[0].plot(t[:K], Z[:K], label = 'noise trades', c = 'gray')
 ax[0].plot(t[:K], Y[:K], label = 'net trades', c = 'C0')
 ax[0].set_ylabel('what the informed \n trader sees', fontsize = 'small')
+ax[0].legend(fontsize = 'xx-small', loc = 'lower right')
 
 ax[1].plot(t[:K], Y[:K], label = 'net trades', c = 'C0')
 ax[1].set_xlabel('time')
 ax[1].set_ylabel('what the market \n maker sees', fontsize = 'small')
-
-ax[0].legend(fontsize = 'xx-small', loc = 'lower right')
 ax[1].legend(fontsize = 'xx-small', loc = 'lower right')
 
 fig.savefig('plots/trades.jpeg', transparent = False, bbox_inches = 'tight')
