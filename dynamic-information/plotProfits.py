60f833a0498bccc6ad8736565a0d7b4d61c5fe58
Lu Vy <luvy@umich.edu>
2025-01-01 14:50:59 -0700

    Update plotProfits.py

--- dynamic-information/plotProfits.py
+++ dynamic-information/plotProfits.py
@@ -18,24 +18,18 @@ M[1:] = np.cumsum((P[1:] - V[-1]) * np.diff(Y))
 
 fig, ax = plt.subplots(nrows = 2, ncols = 1,
                        figsize = (4.5,3.5), dpi = 256,
-                      sharex = 'col')
-ymin = min([J.min(), L.min(), M.min()]) * 1.2
-ymax = max([J.max(), L.max(), M.max()]) * 1.2
+                      sharex = 'col', sharey = True)
 
-K = int(0.75 * len(X))
+K = int(0.75 * N)
 
-ax[0].set_ylim(bottom = ymin, top = ymax)
 ax[0].set_xlim(left = -0.05, right = 1.05)
-ax[0].set_ylabel('profits')
+ax[0].set_ylabel('what the informed \n trader sees', fontsize = 'small')
 ax[0].plot(t[:K], J[:K], label = 'informed profits', c = 'C1')
 ax[0].plot(t[:K], L[:K], label = 'noise profits', c = 'gray')
 ax[0].plot(t[:K], M[:K], label = 'market maker profits', c = 'C0')
-ax[0].text(0,4.75,'what the informed trader sees', color = 'gray')
 
 ax[1].set_xlabel('time')
-ax[1].set_ylabel('profits')
-ax[1].set_ylim(bottom = ymin, top = ymax)
-ax[1].text(0,4.75,'what the market maker sees', color = 'gray')
+ax[1].set_ylabel('what the market \n maker sees', fontsize = 'small')
 ax[1].plot(t[:K], M[:K], label = 'market maker profits', c = 'C0')
 
 ax[0].legend(fontsize = 'xx-small', loc = 'center right')
