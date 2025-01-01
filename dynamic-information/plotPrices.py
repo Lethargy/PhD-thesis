a558c470fe0c081dab176054b5ca9a8637bba45e
Lu Vy <luvy@umich.edu>
2025-01-01 14:44:16 -0700

    Update plotPrices.py

--- dynamic-information/plotPrices.py
+++ dynamic-information/plotPrices.py
@@ -1,23 +1,16 @@
 import numpy as np
-from scipy.stats import norm
 import matplotlib.pyplot as plt
 
 t = np.load('data/t.npy')
-S = np.load('data/S.npy')
 P = np.load('data/P.npy')
-X = np.load('data/X.npy')
-Z = np.load('data/Z.npy')
 V = np.load('data/V.npy')
-p0 = np.load('data/p0.npy')
-lam = np.load('data/lam.npy')
-SIG = np.load('data/SIG.npy')
 N = np.load('data/N.npy')
 
 fig, ax = plt.subplots(nrows = 2, ncols = 1,
                        figsize = (4.5,3.5), dpi = 256,
                       sharex = 'col', sharey = True)
 
-K = int(0.75 * len(t))
+K = int(0.75 * N)
 
 ax[0].set_xlim(left = -0.05, right = 1.05)
 ax[0].set_ylabel('what the informed \n trader sees', fontsize = 'small')
